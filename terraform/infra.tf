68bc10e707cf969e7972ac34040c0c292d82bd97
padmasri-jampana <68692696+padmasri-jampana@users.noreply.github.com>
2020-09-14 13:14:29 +0530

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,9 +1,10 @@
 provider "google" {
   project = "ind-coe"
+  region = "us-central1"
 }
 
-resource "google_storage_bucket" "funky-buckets" {
-  name = "batch-pipelinefirst"
+resource "google_storage_bucket" "bucketone" {
+  name = "batch-pipelineone"
   storage_class = "REGIONAL"
-  location  = "australia-southeast1"
+  location  = "us-central1"
 }
